2175c5eae8d76b52f5198ee12d8b08947ab7add3
peishuenwu@gmail.com <falis@ureach-inc.com>
2026-05-23 12:16:19 +0800

    fix(webauthn): discourage resident keys to bypass mobile biometric prompts

--- src/api/auth.js
+++ src/api/auth.js
@@ -181,7 +181,7 @@ async function handleRegisterOptions(req, res) {
       type: 'public-key',
     })),
     authenticatorSelection: {
-      residentKey: 'preferred',
+      residentKey: 'discouraged',
       requireResidentKey: false,
       userVerification: 'discouraged',
     },
